568fd0d24fcf576d7114b26ade6c0b1804286291
Jeffrey Chen <jcyh0120@gmail.com>
2023-03-03 16:00:46 +0800

    add basic layout for status page

--- src/components/Tab.svelte
+++ src/components/Tab.svelte
@@ -2,48 +2,76 @@
     export let select = 1;
 </script>
 
-<div class="info-board nes-container with-title">
-    <div class="info-card nes-container with-title">
-        <p class="title">{select} 基本資料</p>
+<div class="info-card nes-container with-title">
+    <h2 class="title">{select} 基本資料</h2>
 
-        <div class="info-basic">
-            <div class="name">
-                <p><b>名稱：</b><span>Jeffrey</span></p>
-            </div>
-            <div class="level">
-                <p><b>等級：</b><span>LV34</span></p>
-            </div>
-            <div class="photo">
-                <i class="nes-bcrikko" />
-            </div>
-
-            <div class="sign">
-                <p><b>星座：</b><span>摩羯</span></p>
-            </div>
-
-            <div class="sex">
-                <p><b>性別：</b><span>男</span></p>
-            </div>
+    <div class="info-basic">
+        <div class="name">
+            <p><b>名稱：</b><span>Jeffrey</span></p>
         </div>
-    </div>
+        <div class="level">
+            <p><b>等級：</b><span>LV34</span></p>
+        </div>
+        <div class="photo">
+            <i class="nes-bcrikko" />
+        </div>
 
-    <div class="info-card nes-container with-title">
-        <p class="title">職業</p>
-    </div>
+        <div class="sign">
+            <p><b>星座：</b><span>摩羯</span></p>
+        </div>
 
-    <div class="info-card nes-container with-title">
-        <p class="title">故事</p>
+        <div class="sex">
+            <p><b>性別：</b><span>男</span></p>
+        </div>
     </div>
 </div>
 
-<style>
-    .info-board {
-        padding: 0rem;
-    }
+<div class="info-card nes-container with-title">
+    <h2 class="title">職業</h2>
 
+    <div class="info-job">
+        <div class="guild">
+            <p><b>公會：</b></p>
+            <p>Aeon Matrix</p>
+        </div>
+
+        <div class="position">
+            <p><b>職位：</b></p>
+
+            <p class="is-dark">
+                <i class="nes-icon is-small star" />
+                <span>Fullstack Developer</span>
+            </p>
+
+            <p class="is-dark">
+                <i class="nes-icon is-small star is-half" />
+                <span>Fullstack Developer</span>
+            </p>
+            <p class="is-dark">
+                <i class="nes-icon is-small star is-empty" />
+                <span>Fullstack Developer</span>
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="info-card nes-container with-title">
+    <h2 class="title">故事</h2>
+
+    <p>
+        這是這個人的冒險故事 這是這個人的冒險故事 這是這個人的冒險故事
+        這是這個人的冒險故事 這是這個人的冒險故事 這是這個人的冒險故事
+        這是這個人的冒險故事 這是這個人的冒險故事 這是這個人的冒險故事
+    </p>
+</div>
+
+<style>
     .info-card {
-        margin: 1rem;
-        padding: 1rem;
+        margin: 0.5rem auto;
+        padding: 1.5rem;
+        /* border-bottom: 0;
+        border-left: 0;
+        border-right: 0; */
     }
 
     .info-basic {
@@ -78,5 +106,29 @@
 
     .photo {
         grid-area: photo;
+    }
+
+    .info-job-grid {
+        display: grid;
+        grid-template-columns: minmax(6rem, 10rem) auto;
+        grid-template-rows: auto;
+        grid-template-areas: "guild position";
+        grid-gap: 10px;
+    }
+
+    .info-job {
+        /* display: flex; */
+        justify-content: space;
+    }
+
+    .guild {
+        /* flex: 0 1 auto; */
+        /* border: solid 1px; */
+        /* padding: 0.5rem; */
+    }
+
+    .position {
+        /* border: solid 1px; */
+        margin-bottom: 0.5rem;
     }
 </style>
